52f855978061053b88b75e430a37ed1800d14e54
David Caspar <david.caspar@aol.ch>
2017-01-01 11:49:31 +0100

    Add additional test for -->> with narrower generators.

--- test/algojam/graphs_test.clj
+++ test/algojam/graphs_test.clj
@@ -1,9 +1,22 @@
 (ns algojam.graphs-test
   (:require [clojure.spec.test :as st]
+            [clojure.spec.gen :as gen]
+            [clojure.spec :as s]
             [clojure.test :as t]
-            [algojam.graphs :as g]))
+            [algojam.graphs :as g]
+            [algojam.graphs.generators :as ggen]
+            [clojure.test.check.clojure-test :as tcct]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.generators :as tcgen]))
 
 (t/deftest -->>
-  (let [result (st/check `g/-->> {::clojure.spec.test.check/opts {:num-tests 24}})
-        data   (-> result first :clojure.spec.test.check/ret)]
-    (t/is (true? (:result data)) (str "Random seed is: " (:seed data)))))
+  (t/testing "-->> with very general generators."
+    (let [result (st/check `g/-->> {::clojure.spec.test.check/opts {:num-tests 50}})
+          data   (-> result first :clojure.spec.test.check/ret)]
+      (t/is (true? (:result data)) (str "Random seed is: " (:seed data)))))
+  (t/testing "-->> with more narrowed generators"
+    (let [result (st/check `g/-->> {::clojure.spec.test.check/opts {:num-tests 100}
+                                    :gen                           {::g/node  (fn [] tcgen/nat)
+                                                                    ::g/graph #(ggen/graph-gen tcgen/nat (gen/int))}})
+          data   (-> result first :clojure.spec.test.check/ret)]
+      (t/is (true? (:result data)) (str "Random seed is: " (:seed data))))))
